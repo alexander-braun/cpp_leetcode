7c32a10566675316e7955ad76f1c4d5fc4092bf0
Alex <alexanderbraun@freenet.de>
2021-04-10 21:23:54 +0200

    add thousand_seperator solution

---
+++ Cpp-Leetcode/Thousand_Separator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using std::cout;
+
+#include <string>
+using std::string;
+using std::to_string;
+
+class Solution {
+public:
+    string thousandSeparator(int n) {
+        string ofNum = to_string(n);
+        int len = ofNum.length();
+
+        if (len < 4) {
+            return ofNum;
+        }
+
+        for (int i = len - 3; i > 0; i -= 3) {
+            ofNum.insert(i, ".");
+        }
+
+        return ofNum;
+    }
+};
